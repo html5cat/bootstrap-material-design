e9c677df37ca42d001ca0c80ea291b3befe8fa37
Yuriy Dybskiy <yuriy@dybskiy.com>
2014-09-29 20:45:25 -0700

    Add bootstrap 3.2.0

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  summary: "[Wrapper package] Google's Material Design based Bootstrap 3 theme.",
+  version: "0.1.0",
+  // git: "https://github.com/FezVrasta/bootstrap-material-design.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.3');
+  api.use(['mizzao:bootstrap-3@3.2.0', 'jquery'], 'client');
+
+  api.addFiles([
+    // 'bootstrap-material-design/css-compiled/material-wfont.css',
+    'bootstrap-material-design/css-compiled/material.css',
+    'bootstrap-material-design/css-compiled/ripples.css',
+    'bootstrap-material-design/scripts/ripples.js',
+    'bootstrap-material-design/scripts/material.js',
+    'bootstrap-material-design/icons/icons-material-design.css',
+    'bootstrap-material-design/icons/fonts/Material-Design.eot',
+    'bootstrap-material-design/icons/fonts/Material-Design.svg',
+    'bootstrap-material-design/icons/fonts/Material-Design.ttf',
+    'bootstrap-material-design/icons/fonts/Material-Design.woff'
+    ], 'client', {bare: true});
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('html5cat:bootstrap-material-design');
+  // api.addFiles('html5cat:bootstrap-material-design-tests.js');
+});
